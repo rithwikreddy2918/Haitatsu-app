0f80645546f3baee4dd3b105d3841bfa47a64935
rithwikreddy2918 <rithwikreddy2906@gmail.com>
2025-10-13 14:05:12 +0530

    admin

---
+++ Haitatsu-Backend/controller/userController.js
@@ -0,0 +1,38 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+const loginUser= async(req,res)=>{
+
+}
+const createToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+const registerUser=async(req,res)=>{
+    const {name,email,password}=req.body;
+    try{
+        const exits=await userModel.findOne({email});
+        if(exits){
+            return res.json({success:false,message:"user already exits"})
+        } if(!validator.isEmail(email)){
+            return res.json({success:false,message:"invalid email"})
+        } if(password.length<8){
+            return res.json({success:false,message:"password must be 8 character"})
+        }
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
+        const newUser=new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        });
+         const user = await newUser.save();
+        const token=createToken(user._id);
+        res.json({success:true,token});
+    }catch(error){
+        console.log(error);
+        res.json({success:false,message:"something went wrong"})
+    }
+}
+export {loginUser,registerUser}
